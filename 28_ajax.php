0197e99a809c4b062ed4588349148ed6e8b8938d
LanhPham <lanhphamthi@gmail.com>
2025-08-05 20:10:14 +0700

    add 28

--- 28_ajax.php
+++ 28_ajax.php
@@ -1,17 +1,35 @@
-<?php 
+<?php
+include_once('17_connectDB.php');
+if (isset($_GET['name'])) {
+    $name = $_GET['name'];
 
-    if (isset($_GET['name'])) {
-    // $name = $_GET['data'];
-    // //viet cau lenh truy van
-    // $query = "SELECT * FROM tbStudent WHERE stuName LIKE '%" . $name. "%' ";
-    // //thuc thi truy van
-    // $result2 = mysqli_query($cn, $query);
+    $cn = connect();
+    //viet cau lenh truy van
+    $query = "SELECT * FROM tbStudent WHERE stuName LIKE '%" . $name . "%' ";
+    //thuc thi truy van
+    $result2 = mysqli_query($cn, $query);
 
-    // //khai bao 1 mang rong de chua du lieu trich xuat ra tu bang ket qua
-    // $data = [];
+    //khai bao 1 mang rong de chua du lieu trich xuat ra tu bang ket qua
+    $data = [];
 
-    // while ($row = mysqli_fetch_assoc($result2)) {
-    //     $data[] = $row; //va them vao mang data
-    // }
-    echo "OK";
-}
+    while ($row = mysqli_fetch_assoc($result2)) {
+        $data[] = $row; //va them vao mang data
+    } ?>
+
+    //dong ket noi
+    disconnect($cn);
+
+     <?php foreach ($data as $item) { ?>
+        <tr>
+            <td><?php echo $item['stuID'] ?></td>
+            <td><?php echo $item['stuName'] ?></td>
+            <td><?php echo $item['stuGender'] ?></td>
+            <td><?php echo $item['stuDOB'] ?></td>
+            <td>
+                <a href="20_student_edit.php?id=<?php echo $item['stuID'] ?>">Edit</a> |
+                <a href="21_student_delete.php?id=<?php echo $item['stuID'] ?>"
+                    onclick="return confirm('Are u sure to delete?')">Delete</a>
+            </td>
+        </tr>
+            <?php } ?>
+<?php } ?>
